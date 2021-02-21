13d164bb7b74ca3e81dd9dd7a0b71b39c51a2011
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-02-21 15:43:28 +0200

    handling error

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use('*', (req, res) => {
 	});
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
 });
